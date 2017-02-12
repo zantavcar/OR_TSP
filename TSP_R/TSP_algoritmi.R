1f98e14e9257fb68dbf6dd042e17ab641307a54e
Žan Tavčar <press.zan.tavcar@gmail.com>
2017-02-12 17:34:29 +0100

    big update
    big update

--- TSP_R/TSP_algoritmi.R
+++ TSP_R/TSP_algoritmi.R
@@ -1,5 +1,7 @@
 require(dplyr)
 require(lpSolve) #resevanje Madzarske metode
+require(xtable)
+require(ggplot2)
 ### HEVRISTICNE METODE TSP
 simetricna <- function(n){ #matrika, ce predpostavimo simetricni TSP, n je st. vozlisc
   A <- matrix(rep(Inf,n*n),ncol = n,nrow = n)
@@ -50,6 +52,7 @@ najblizji_sosed <- function(A,z){ #TSP po metodi najblizjih sosedov, funkcija vr
   }
   
   pot <- append(pot,zacetek)
+  teza <- teza(A,pot)
   return(pot)
 }
 
@@ -118,23 +121,27 @@ reversal <- function(A,zacetna=najblizji_sosed(A,opt_zacetek(A))){ #kombinirana 
 }
 
 ### EKSAKTNI ALGORITMI (BB algoritem)
-permutacije <- function(A){ #vhod je matrika velikosti nx2 (rešitev nxn assignment problema), izhod
+permutacije <- function(A){ #vhod je matrika, izhod pa permutacije (rešitev assingment problem)
   # pa list ciklov
-  n=nrow(A)
+  A[!is.finite(A)] <- 1000 #nastavimo dovolj visoko, saj lp.assign ne zna delati z Inf, ne pa preveč,
+  #ker pride do težav iz neznanih razlogov
+  C <-lp.assign(A,direction = "min")$solution
+  resitev <- which(!C == 0,arr.ind = TRUE)
+  n=nrow(resitev)
   perm <- list()
-  Q=A[,1]
+  Q=resitev[,1]
   i=1 #stevec da dodajamo v list
   while (!length(Q)==0){
     cikel <- c()
     zacetek <- Q[1]
-    cikel <- append(cikel,zacetek)
-    Q <- setdiff(Q,zacetek)
-    indeks <- which(A[,1]==zacetek)
-    while (!A[indeks,2]==zacetek){
-      j <- A[indeks,2]
+    cikel <- append(cikel,zacetek) 
+    Q <- setdiff(Q,zacetek) #vozlisce odstranimo iz Q
+    indeks <- which(resitev[,1]==zacetek)
+    while (!resitev[indeks,2]==zacetek){
+      j <- resitev[indeks,2]
       cikel <- append(cikel,j)
       Q <- setdiff(Q,j)
-      indeks <- which(A[,1]==j)
+      indeks <- which(resitev[,1]==j)
     }
     cikel <- as.integer(append(cikel,zacetek))
     perm[[i]] <- cikel
@@ -154,7 +161,8 @@ indeks_cikla <- function(l){ #indeks minimalnega podcikla v list
   
 }
 
-teza_permutacij <- function(A,resitev){ #vhod je lista permutacij
+teza_permutacij <- function(A){ #vhod je matrika
+  resitev <- permutacije(A)
   n <- length(resitev)
   cena <- 0
   for (i in 1:n){
@@ -165,61 +173,49 @@ teza_permutacij <- function(A,resitev){ #vhod je lista permutacij
 }
 
 veja <- function(A){ #resevanje enega nivoja za eno vejo BB algoritma, vrne bodisi resitev, bodisi podproblem
-  A[!is.finite(A)]<-100 #nastavimo inf na velike vrednosti, lpSolve ne zna delati z inf
-  #IN NITI Z "PRE"VELIKIMI VREDNOSTMI!!!
-  C <-lp.assign(A,direction = "min")$solution
-  resitev <- permutacije(which(!C == 0,arr.ind = TRUE))
-  upper <- reversal(A)$teza
+  resitev <- permutacije(A)
+  lista_matrik <- list()
   if (length(resitev)==1){ #ce imamo en sam cikel, smo dobili obhod, vrnemo pot in ceno poti
-    cena <- teza_permutacij(A,resitev)
+    cena <- teza_permutacij(A)
     pot <- paste0(as.character(resitev[[1]]),collapse = "-")
-    if (cena <= upper){
-      upper <- cena #NIZANJE UPPER LIMIT
-    return(c(pot,cena))
-    }
-    else{
-      return(c(""))
-    }
+    lista_matrik[[1]]<-c(cena,pot)
   }
   else{ #sicer razbivamo najmanjsi cikel, dobimo vec manjsih podproblemov
-  cikel <- resitev[[indeks_cikla(resitev)]]
-  lista_matrik <- list()
+  cikel <- resitev[[indeks_cikla(resitev)]] #določitev cikla z najmnajšo dolžino
   for (i in (1:(length(cikel)-1))){
     B <- A
-    B[cikel[i],cikel[i+1]] <- 100
-    B1 <- lp.assign(B)$solution
-    resitev1 <- cena <- teza_permutacij(A,permutacije(which(!B1 == 0,arr.ind = TRUE)))
-    if (resitev1 < upper){
-    lista_matrik[[i]]<-B  
-    }
-    else{
-      lista_matrik[[i]] <- c("") #ZAPIRANJE VEJE, CE JE LE TA ZE NAD RESITVIJO IZ ALGORITMA REVERSAL
-    }
-    
-  
+    B[cikel[i],cikel[i+1]] <- Inf
+    lista_matrik[[i]] <- B
+  }
   }
   return(lista_matrik)
-  }
 }
 
-nivo <- function(A){#vhod je nek list, v katerem imamo class character in matrix.
+nivo <- function(A){#vhod je nek list, v katerem imamo class character (že rešeno) in matrix.
   n <- length(A) #dolzina nivoja
   lista <- list()
+  zgornja <- 10000
   for (i in (1:n)){
     if (is.character(A[[i]])){
-      lista[[i]] <- A[[i]] #kar smo že rešili samo prepišemo
+      lista[[i]] <- A[[i]]
     }
     else{
       resitev <- veja(A[[i]])
-      if (is.character(resitev)){
-        lista[[i]] <- resitev
-      }
-      else{ # dodajanje matrike v resitev
-        a=length(resitev) #na koliko vej se cepi
-        for (k in (0:(a-1))){
-          lista[[i+k]]<-resitev[[k+1]]
+      a <- length(resitev)
+      for (j in 1:a){
+        if (is.character(resitev[[j]])){
+          lista[[i+j-1]] <- resitev[[j]]
         }
-        n <- n + (a-1)
+        else{
+          if (zgornja > teza_permutacij(resitev[[j]])){
+          lista[[i+j-1]] <- resitev[[j]]
+          zgornja <- teza_permutacij(resitev[[j]])
+          }
+          else {
+            lista[[i+j-1]] <- c("")
+          }
+        }
+        n <- n + a - 1
       }
     }
   }
@@ -231,100 +227,132 @@ nivo <- function(A){#vhod je nek list, v katerem imamo class character in matrix
 
 bb <- function(A){
   k <- veja(A)
+  if (length(k)==1){ #že v štartu dobimo cikel in ni treba delat vej naprej
+    return(c(k[[1]][1],k[[1]][2]))
+  }
   logicne <- c(FALSE)
   while (!all(logicne)){
     k<-nivo(k)
     n <- length(k)
     logicne <- c()
+
   for (i in 1:n){
-    logicne[i] <- is.character(k[[i]])
+    logicne[i] <- is.character(k[[i]][1])
   }
 
   }
   pot <- c()
   cena <- c()
-  min <- reversal(A)$teza
   for (j in 1:n){
-    if ((!is.na(as.integer(k[[j]][2]))) && (as.integer(k[[j]][2])<=min)){
-      cena <- as.integer(k[[j]][2])
-      min <- cena
-      pot <- k[[j]][1]
-    }
-    else{
-      next
-    }
-    
+  pot <- append(pot,k[[j]][2])
+  cena <- append(cena,as.numeric(k[[j]][1]))
   }
- return(c(pot,cena))
+  indeks <- which.min(cena)
+ return(c(pot[indeks],cena[indeks]))
 }
   
 ###PRIMERJAVE METOD
 
 
-primerjava_najblizji <- function(m){#primerjajmo kako dobra je metoda najbliznjih
-  #sosedov, naredimo m simulacij in gledamo relativno napako
-  relativna <- c()
+primerjava <- function(m){#primerjajmo kako dobra je metoda najblizjih
+  #sosedov, naredimo m simulacij in gledamo relativno napako, zacnemo v random tocki
+  relativna1 <- c()
+  relativna2 <- c()
+  relativna3 <- c()
   for (i in 1:m){
+    
     A <- simetricna(10)
     nakljucni_zacetek <- sample(1:10,1)
     prava_vrednost <- as.numeric(bb(A)[2])
-    pot_sosed <- najblizji_sosed(A,nakljucni_zacetek)
-    pot_teza <- teza(A,pot_sosed)
-    napaka <- abs(pot_teza-prava_vrednost)/prava_vrednost
-    relativna <- append(relativna,napaka)
-  }
-  povp <- mean(relativna)
-  histogram <- hist(relativna,probability = TRUE,main = paste0("Relativna napaka metode najbližjih sosedov"),
-                    xlab = "Relativna napaka",ylab = "Gostota",breaks=30)
-  histogram <- abline(v=mean(relativna),col="red",lwd=2)
-  histogram <- legend("topright",col=c("red"), lwd=2,
-                      legend=c(paste0("Povprečje = ",round(povp,2))), cex=0.8)
-  
-  return(histogram)
-}
+    pot_sosed1 <- najblizji_sosed(A,nakljucni_zacetek)
+    pot_teza1 <- teza(A,pot_sosed1)
+    napaka1 <- abs(pot_teza1-prava_vrednost)/prava_vrednost
+    relativna1 <- append(relativna1,napaka1)
 
-primerjava_optnajblizji <- function(m){#primerjajmo kako dobra je metoda najbliznjih
-  #sosedov, ce zacnemo v vozliscu, ki je optimalno. naredimo m simulacij in gledamo relativno napako
-  relativna <- c()
-  for (i in 1:m){
-    A <- simetricna(10)
     zacetek <- opt_zacetek(A)
-    prava_vrednost <- as.numeric(bb(A)[2])
-    pot_sosed <- najblizji_sosed(A,zacetek)
-    pot_teza <- teza(A,pot_sosed)
-    napaka <- abs(pot_teza-prava_vrednost)/prava_vrednost
-    relativna <- append(relativna,napaka)
+    pot_sosed2 <- najblizji_sosed(A,zacetek)
+    pot_teza2 <- teza(A,pot_sosed2)
+    napaka2 <- abs(pot_teza2-prava_vrednost)/prava_vrednost
+    relativna2 <- append(relativna2,napaka2)
+
+    pot_teza3 <- as.numeric(reversal(A)$teza)
+    napaka3 <- abs(pot_teza3-prava_vrednost)/prava_vrednost
+    relativna3 <- append(relativna3,napaka3)
+
   }
-  povp <- mean(relativna)
-  histogram <- hist(relativna,probability = TRUE,main = paste0("Relativna napaka opt. metode najbližjih sosedov"),
-                    xlab = "Relativna napaka",ylab = "Gostota",breaks = 30)
-  histogram <- abline(v=mean(relativna),col="red",lwd=2)
-  histogram <- legend("topright",col=c("red"), lwd=2,
-                      legend=c(paste0("Povprečje = ",round(povp,2))), cex=0.8)
+  povp1 <- mean(relativna1)
+  histogram1 <- hist(relativna1,probability = TRUE,main = paste0("Relativna napaka metode najbližjih sosedov"),
+                     xlab = "Relativna napaka",ylab = "Gostota",breaks=30)
+  histogram1 <- abline(v=povp1,col="red",lwd=2)
+  histogram1 <- legend("topleft",col=c("red"), lwd=2,bty="n",xjust=1, seg.len=0.5,
+                       legend=c(paste0("Povprečje = ",round(povp1,4))), cex=0.8)
   
-  return(histogram)
+  povp2 <- mean(relativna2)
+  histogram2 <- hist(relativna2,probability = TRUE,main = paste0("Relativna napaka opt. metode najbližjih sosedov"),
+                     xlab = "Relativna napaka",ylab = "Gostota",breaks = 30)
+  histogram2 <- abline(v=povp2,col="red",lwd=2)
+  histogram2 <- legend("topleft",col=c("red"), lwd=2,bty="n",xjust=1, seg.len=0.5,
+                       legend=c(paste0("Povprečje = ",round(povp2,4))), cex=0.8)
+  
+  povp3 <- mean(relativna3)
+  histogram3 <- hist(relativna3,probability = TRUE,main = paste0("Relativna napaka metode obratov"),
+                     xlab = "Relativna napaka",ylab = "Gostota",breaks = 30)
+  histogram3 <- abline(v=povp3,col="red",lwd=2)
+  histogram3 <- legend("topleft",col=c("red"), lwd=2,bty="n",xjust=1, seg.len=0.5,
+                       legend=c(paste0("Povprečje = ",round(povp3,4))), cex=0.8)
+  
+  par(mfrow=c(3,1))
+  histogram1
+  histogram2
+  histogram3
+  return()
 }
 
-primerjava_reversal <- function(m){#primerjajmo kako dobra je metoda reversal
-  #naredimo m simulacij in gledamo relativno napako
-  relativna <- c()
-  for (i in 1:m){
-    A <- simetricna(10)
-    prava_vrednost <- as.numeric(bb(A)[2])
-    pot_teza <- as.numeric(reversal(A)$teza)
-    napaka <- abs(pot_teza-prava_vrednost)/prava_vrednost
-    relativna <- append(relativna,napaka)
-  }
-  povp <- mean(relativna)
-  histogram <- hist(relativna,probability = TRUE,main = paste0("Relativna napaka metode obratov"),
-                    xlab = "Relativna napaka",ylab = "Gostota",breaks = 30)
-  histogram <- abline(v=mean(relativna),col="red",lwd=2)
-  histogram <- legend("topright",col=c("red"), lwd=2,
-                      legend=c(paste0("Povprečje = ",round(povp,2))), cex=0.8)
+### RAČUNANJE ČASA ZA IZVEDBO PROGRAMA PRI RAZLIČNIH VELIKOSTIH MATRIKE
+
+casovna_zahtevnost <- function(n){ #gledali bomo, koliko casa porabimo za izvedbo
+  #TSP z razlicnimi algoritmi in kako se cas spreminja z vecanjem velikosti matrike
+  # pri vsaki velikosti bomo z vsako metodo (sosedi,reversal,BB) po 10-krat izracunali
+  #cas izvajanja ter vzeli povprecnega. Velikost matrike bo tekla od 1x1 do 20x20
+  #gradili bomo tabelo, za kasnjejse delo z dplyr in lazjo graficno predstavitev
+  #Tidy data (velikost matrike, tip resevanja, povp cas)
+  velikost <- c()
+  tip <- c() #nacin resevanja ("sosed","rev","bb")
+  cas_tabela <- c()
   
-  return(histogram)
+  for (s in 2:n){
+    cas_sosed_sim <- c()
+    cas_reversal_sim <- c()
+    cas_bb_sim <- c()
+    for (j in 1:100){
+    A <- simetricna(s) #simulacija matrike
+    z <- opt_zacetek(A)
+      #NAJBLIZJI SOSED
+      start1 <- proc.time()
+      k <- najblizji_sosed(A,z)
+      konec1 <- proc.time()
+      cas1 <- (konec1-start1)[1]
+      cas_sosed_sim <- append(cas_sosed_sim,cas1)
+      #REVERSAL
+      start2 <- proc.time()
+      reversal(A,k)
+      konec2 <- proc.time()
+      cas2 <- (konec2-start2)[1]
+      cas_reversal_sim <- append(cas_reversal_sim,cas2+cas1) #kaj je optimalna pot,
+      #smo ze poracunali, vendar je to vhod pri reversal metodi, zato pristejemo tudi cas1
+      #BB ALGORITEM
+      start3 <- proc.time()
+      bb(A)
+      konec3 <- proc.time()
+      cas3 <- (konec3-start3)[1]
+      cas_bb_sim <- append(cas_bb_sim,cas3)
+    }
+    velikost <- append(velikost,rep(s,3))
+    tip <- append(tip,c("sosed","rev","bb"))
+    cas_tabela <- append(cas_tabela,c(mean(cas_sosed_sim),mean(cas_reversal_sim),mean(cas_bb_sim)))
+  }
+  return(data_frame(velikost,tip,cas_tabela))
 }
 
-uvoz <- function(){ #uvozi podatke razdalj med 21 mesti v Evropi
-  return(as.matrix(eurodist))
-}
+#graf <- ggplot(data = tabela2,aes(x =velikost,y=cas_tabela,colour=tip)) + geom_point() + geom_line() + xlab("Velikost matrike")+
+#  ylab("Časovna zahtevnost") + ggtitle("Časovna zahtevnost v odvisnosti od velikosti matrike")
